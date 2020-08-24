366b15cf0d3de3986f43384fe71f60d496b5c03a
SFort <github@ssf.tf>
2020-08-24 06:12:43 +0200

    Optimized config loading

--- src/main/java/sf/ssf/sfort/SkinShine.java
+++ src/main/java/sf/ssf/sfort/SkinShine.java
@@ -6,6 +6,7 @@ import net.fabricmc.loader.api.FabricLoader;
 import java.io.File;
 import java.nio.file.Files;
 import java.util.*;
+import java.util.logging.Logger;
 
 public class SkinShine implements ClientModInitializer {
 
@@ -18,11 +19,7 @@ public class SkinShine implements ClientModInitializer {
     public static boolean shouldSelfHide(){ return keepSelfHidden; }
     @Override
     public void onInitializeClient() {
-
-        // Configuration
-        // If anyone is wondering why this code is very odd
-        // for some reason i could not stop thinking of the instruction path length
-        // spent way to much time on it, then threw it away
+        // Configs
         File confFolder = FabricLoader.getInstance().getConfigDirectory();
         File oldFile = new File(
                 confFolder,
@@ -40,30 +37,24 @@ public class SkinShine implements ClientModInitializer {
                     "^-Should your armor always be invis? [false] true | false"
             );
             String[] ls = la.toArray(new String[la.size()>defaultDesc.size()*2?la.size():defaultDesc.size()*2]);
-            for (int i = 0; i<defaultDesc.size()*2;i++)
-                if ((i&1)==1){
-                    ls[i]= defaultDesc.get(i/2);
-                }else{
-                    switch (i) {
-                        case 0:
-                            try{ showAt =Float.parseFloat(ls[0]);}catch (NumberFormatException ignored){};
-                            ls[0] = String.valueOf(showAt);
-                            break;
-                        case 2:
-                            keepSelfHidden = ls[2].contains("true");
-                            ls[2] = String.valueOf(keepSelfHidden);
-                            break;
-                    }
-                }
+            for (int i = 0; i<defaultDesc.size();++i)
+                ls[i*2+1]= defaultDesc.get(i);
+
+            try{ showAt =Float.parseFloat(ls[0]);}catch (NumberFormatException ignored){};
+            ls[0] = String.valueOf(showAt);
+
+            keepSelfHidden = ls[2].contains("true");
+            ls[2] = String.valueOf(keepSelfHidden);
+
             if(ls.length>defaultDesc.size()*2){
                 for (int i = (defaultDesc.size()*2)+1; i<=ls.length;i+=2){
                     ls[i] = "!#Unknown value / config from the future";
                 }
             }
             Files.write(confFile.toPath(), Arrays.asList(ls));
-
+            System.out.println("tf.ssf.sfort.skinshine successfully loaded config file");
         } catch(Exception e) {
-            System.out.println("tf.ssf.sfort.skinshine failed to load config file, using defaults\n\n"+e);
+            System.out.println("tf.ssf.sfort.skinshine failed to load config file, using defaults\n"+e);
         }
     }
 }
